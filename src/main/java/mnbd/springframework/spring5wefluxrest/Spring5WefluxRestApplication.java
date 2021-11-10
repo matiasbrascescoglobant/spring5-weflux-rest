d5caf97373c2ee0d51656adc32f515df2dd08680
matiasbrascescoglobant <matias.brascesco@globant.com>
2021-11-10 12:31:03 -0300

    first commit

---
+++ src/main/java/mnbd/springframework/spring5wefluxrest/Spring5WefluxRestApplication.java
@@ -0,0 +1,13 @@
+package mnbd.springframework.spring5wefluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WefluxRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WefluxRestApplication.class, args);
+	}
+
+}
